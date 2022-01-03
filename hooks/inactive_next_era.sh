74dc947d43cf026340336fd9df06430bd59faeca
Paulo <paulo.rmartins@gmail.com>
2022-01-03 22:36:03 +0000

    fix scripts

--- hooks/inactive_next_era.sh
+++ hooks/inactive_next_era.sh
@@ -10,4 +10,14 @@
 # 4th - Next Era
 # 5th - Next Session 
 #
+# > Special character '!' controls message visibility on Matrix (Element)
+# Any message that starts with '!' will be sent to Matrix, to the user private room
+# 
+# echo "! This message will be sent to Matrix"
+# echo "This message will NOT be sent to Matrix"
+# 
+# ***** START *****
+#
 echo "🟡 $1 -> INACTIVE Next Era $4"
+#
+# ***** END *****
--- hooks/new_session.sh
+++ hooks/new_session.sh
@@ -10,9 +10,20 @@
 # 4th - Session
 # 5th - Era session index
 #
+# > Special character '!' controls message visibility on Matrix (Element)
+# Any message that starts with '!' will be sent to Matrix, to the user private room
+# 
+# echo "! This message will be sent to Matrix"
+# echo "This message will NOT be sent to Matrix"
+# 
+# ***** START *****
+#
 if [ "$2" = "true" ]
 then
-  echo "🟢 $1 -> Session $4 ($5) | Era $3"
+  echo "!🟢 $1 -> Session $4 ($5) | Era $3"
 else
-  echo "🔴 $1 -> Session $4 ($5) | Era $3"
+  echo "!🔴 $1 -> Session $4 ($5) | Era $3"
 fi
+#
+# ***** END *****
+
