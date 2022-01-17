dc5dd5a7a7640f03ab4ff437cde9c74f6036e355
Paulo <paulo.rmartins@gmail.com>
2022-01-17 13:49:43 +0000

    fix 1kv nominators script

--- hooks.examples/1kv/check_1kv_nominators.sh
+++ hooks.examples/1kv/check_1kv_nominators.sh
@@ -6,17 +6,19 @@
 # apt install jq
 #
 # > Make a file executable
-# chmod +x ./nominators.sh
+# chmod +x ./check_1kv_nominators.sh
 # 
 # > Positional arguments:
 # 1st - Validator is active? (true/false)
 # 2nd - Validator nominators
 #
 # > run with the following parameters e.g.:
-# ./nominators.sh true stash_1,stash_2
+# ./check_1kv_nominators.sh true stash_1,stash_2
 # 
 
 FILENAME="$(basename $0)"
+NOMINATORS_1KV_ENDPOINT="https://kusama.w3f.community/nominators"
+NOMINATORS_1KV_RAW_FILENAME="$(dirname $0)/1kv_nominators.json"
 
 printf "> $FILENAME $1 $2 \n"
 
@@ -38,19 +40,27 @@ else
   NOMINATORS=$2
 fi
 
-for row in $( curl 'https://kusama.w3f.community/nominators' | jq -r '.[] | @base64' ); do
+# Write 1kv nominators endpoint to a file
+curl -sS --fail -X GET -G -H 'Accept: application/json' $NOMINATORS_1KV_ENDPOINT -o $NOMINATORS_1KV_RAW_FILENAME 
+if [ $? -ne 0 ]; 
+then
+  printf "! ⚠️ 1kv endpoint is down ($NOMINATORS_1KV_ENDPOINT) \n"
+fi
+
+# Read file and check if one of the 1kv nominators is currently in the list of the validator nominators
+for row in $( cat $NOMINATORS_1KV_RAW_FILENAME | jq -r '.[] | @base64' ); do
     _jq() {
      echo ${row} | base64 --decode | jq -r ${1}
     }
     NOMINATOR=$(_jq '.stash')
-    if [[ "$2" == *"$NOMINATOR"* ]]; then
+    if [[ "$NOMINATORS" == *"$NOMINATOR"* ]]; then
         printf "! 1KV -> $NOMINATOR"
         exit 0
     fi
 done
 
 # 1KV nominators not found and validator active
-if [ "$1" == "true" ]; 
+if [ "$IS_ACTIVE" == "true" ]; 
 then
   printf "! 🥳 Running Independent 🚀 \n"
 fi
--- hooks.examples/_init.sh
+++ hooks.examples/_init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # > make a file executable
-# chmod +x ./_new_era.sh
+# chmod +x ./_init.sh
 #
 # > positional arguments:
 # 1st - Stash
@@ -12,16 +12,17 @@
 # 6th - Era
 # 7th - Session
 # 8th - Eras session index [1,2,3,4,5,6]
+# 9th - Current block
 #
 # The following arguments depend on exposed flags
-# 9th - Network name (--expose-network flag must be set)
-# 10th - Network token symbol (--expose-network flag must be set)
-# 11th - Network token decimals (--expose-network flag must be set)
-#
-# 12th - Validator Total stake (--expose-nominators flag must be set)
-# 13th - Validator Own stake (--expose-nominators flag must be set)
-# 14th - Nominator stashes [stash_1, stash_2, ..] (--expose-nominators flag must be set)
-# 15th - Nominator stakes [stake_1, stake_2, ..] (--expose-nominators flag must be set)
+# 10th - Network name (--expose-network flag must be set)
+# 11th - Network token symbol (--expose-network flag must be set)
+# 12th - Network token decimals (--expose-network flag must be set)
+
+# 13th - Validator Total stake (--expose-nominators flag must be set)
+# 14th - Validator Own stake (--expose-nominators flag must be set)
+# 15th - Nominator stashes [stash_1, stash_2, ..] (--expose-nominators flag must be set)
+# 16th - Nominator stakes [stake_1, stake_2, ..] (--expose-nominators flag must be set)
 #
 # > Special character '!' controls message visibility on Matrix (Element)
 # Any message that starts with '!' will be sent to Matrix, to the user private room
@@ -42,18 +43,21 @@
 # echo "! 6th - Era -> $6"
 # echo "! 7th - Session -> $7"
 # echo "! 8th - Eras session index -> $8"
-# echo "! (9th) - Network name -> ${9}"
-# echo "! (10th) - Network token symbol -> ${10}"
-# echo "! (11th) - Network token decimals -> ${11}"
-# echo "! (12th) - Validator total stake -> ${12}"
-# echo "! (13th) - Validator own stake -> ${13}"
-# echo "! (14th) - Nominators -> ${14}"
-# echo "! (15th) - Nominators Stake -> ${15}"
+# echo "! 9th - Current block -> $9"
+# echo "! (10th) - Network name -> ${10}"
+# echo "! (11th) - Network token symbol -> ${11}"
+# echo "! (12th) - Network token decimals -> ${12}"
+# echo "! (13th) - Validator total stake -> ${13}"
+# echo "! (14th) - Validator own stake -> ${14}"
+# echo "! (15th) - Nominators -> ${15}"
+# echo "! (16th) - Nominators Stake -> ${16}"
 # echo "! -------------------------------"
-#
-# NOTE: this example requires both flags --expose-network --expose-nominators to be present when runing scouty cli
-# /opt/scouty-cli/scouty --config-path /opt/scouty-cli/.env --expose-network --expose-nominators
-#
+# if [ "$4" = "true" ]
+# then
+#   echo "! 🟢 -> 😎"
+# else
+#   echo "! 🔴 -> 😤"
+# fi
 # Convert nominators string "stash_1,stash_2" to an array ("stash_1" "stash_2")
 NOMINATORS=(${15//,/ })
 echo "! Total Nominators ${#NOMINATORS[@]}"
